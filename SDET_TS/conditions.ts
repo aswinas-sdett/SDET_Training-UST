8357fb0bdedf0b22f75c27bf88805591968b9dc8
aswin <aswin.as.sdett@gmail.com>
2026-03-26 22:15:24 +0530

    removed unnecessary files

--- SDET_TS/conditions.ts
+++
@@ -1,9 +0,0 @@
-let time:number = 10;
-if (time == 9)
-{   
-    console.log("welcome to SDET")
-}
-else
-{
-    console.log("training initiated")
-}
--- SDET_TS/h.ts
+++
@@ -1,5 +0,0 @@
-let stringg:string = "hello"
-let s:number =2
-let l:string = "o"
-result = stringg.replace(/${l}/g,"")
-console.log(result)

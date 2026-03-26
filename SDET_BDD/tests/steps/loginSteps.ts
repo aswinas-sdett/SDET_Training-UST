7c63648362c78012010790e22996a850bdda9683
aswin <aswin.as.sdett@gmail.com>
2026-03-26 22:32:55 +0530

    File updated

--- SDET_BDD/tests/steps/loginSteps.ts
+++ SDET_BDD/tests/steps/loginSteps.ts
@@ -9,63 +9,75 @@ const {Given,When,Then} = createBdd();
 
 */
 
-//  Description: Steps for Scenario: Log in with valid credentials
 
-// Given('User is on login page', async ({page}) => {
 
-//     await page.goto("https://www.saucedemo.com/")
-// });
+/*
+ Description: Steps for Scenario: Log in with valid credentials
 
-// When('User enters username', async ({page}) => {
+Given('User is on login page', async ({page}) => {
 
-//   await page.locator("#user-name").fill("standard_user");
-// });
+    await page.goto("https://www.saucedemo.com/")
+});
 
-// When('User enters password', async ({page}) => {
+When('User enters username', async ({page}) => {
+
+  await page.locator("#user-name").fill("standard_user");
+});
+
+When('User enters password', async ({page}) => {
   
-//   await page.locator("#password").fill("secret_sauce");
-// });
+  await page.locator("#password").fill("secret_sauce");
+});
 
-// When('User clicks Login button', async ({page}) => {
+When('User clicks Login button', async ({page}) => {
 
-//   await page.locator("#login-button").click();
-// });
+  await page.locator("#login-button").click();
+});
 
-// Then('User is logged in successfully', async ({page}) => {
+Then('User is logged in successfully', async ({page}) => {
 
-//    await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
+   await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
 
-// });
+});
 
-// Then('User is able to navigate to product page', async ({page}) => {    
+Then('User is able to navigate to product page', async ({page}) => {    
  
-//   await expect(page.locator(("span.title"))).toHaveText("Products");
-// });
+  await expect(page.locator(("span.title"))).toHaveText("Products");
+});
+*/
 
-// //  Description: Steps for Scenario: Log in with locked out user credentials
 
-// When('User enters locked out username', async ({ page }) => {
-//     await page.locator("#user-name").fill("locked_out_user");
-// });
 
-// Then('User gets error message', async ({ page }) => {
-//   await expect(page.locator("//h3[@data-test ='error']")).toHaveText("Epic sadface: Sorry, this user has been locked out.")
-// });
+/*
+Description: Steps for Scenario: Log in with locked out user credentials
 
-// Then('User is still on login page', async ({ page}) => {
-//   await expect(page).toHaveURL("https://www.saucedemo.com");
-// });
+When('User enters locked out username', async ({ page }) => {
+    await page.locator("#user-name").fill("locked_out_user");
+});
 
-// // Description: Steps for Scenario: Log in with problem user credentials
-// When('User enters problem username', async ({ page }) => {
-//     await page.locator("#user-name").fill("problem_user");
-// });
+Then('User gets error message', async ({ page }) => {
+  await expect(page.locator("//h3[@data-test ='error']")).toHaveText("Epic sadface: Sorry, this user has been locked out.")
+});
 
-// Then('The image of product is an image of a dog', async ({ page }) => {
-//  const imagedog = page.locator('.inventory_item_img img').nth(0);
-//  await expect(await imagedog.getAttribute('src')).toBe('/static/media/sl-404.168b1cce10384b857a6f.jpg')
-// //  console.log(await imagedog.getAttribute('src'))
-// });
+Then('User is still on login page', async ({ page}) => {
+  await expect(page).toHaveURL("https://www.saucedemo.com");
+});
+
+*/
+
+
+/*
+Description: Steps for Scenario: Log in with problem user credentials
+When('User enters problem username', async ({ page }) => {
+    await page.locator("#user-name").fill("problem_user");
+});
+
+Then('The image of product is an image of a dog', async ({ page }) => {
+ const imagedog = page.locator('.inventory_item_img img').nth(0);
+ await expect(await imagedog.getAttribute('src')).toBe('/static/media/sl-404.168b1cce10384b857a6f.jpg')
+
+});
+*/
 
 
 //  --------------------------------------------------------------------------------------
@@ -95,11 +107,14 @@ Then('User is logged in successfully', async ({ page }) => {
   await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
 });
 
+
+//checks the page header to match "Products"
 Then('User is able to navigate to product page', async ({ page }) => {
   await expect(page.locator(("span.title"))).toHaveText("Products");
 });
 
-// For problem_user
+// For problem_user 
+//  the thumbnail of all product is replaced by image of a dog
 Then('The image of product is an image of a dog', async ({ page }) => {
  const imagedog = page.locator('.inventory_item_img img').nth(0);
  await expect(await imagedog.getAttribute('src')).toBe('/static/media/sl-404.168b1cce10384b857a6f.jpg')
